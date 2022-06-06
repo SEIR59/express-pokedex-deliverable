6196fa1b6a761a25552a2a91889c99b2061b1042
patrickbasham <PatrickTBasham@gmail.com>
2022-06-06 18:23:22 -0400

    set up GET routes and added comments

--- pokedex/server.js
+++ pokedex/server.js
@@ -6,8 +6,10 @@ const app = require("liquid-express-views")(expr())
 const methodOverride = require("method-override")
 
 const port = 3000
-const rowdy = require("rowdy-logger")
+const rowdy = require("rowdy-logger") // set up rowdy-logger
 const rowdyLogger = rowdy.begin(app)
+
+const arrPokemon = require("./models/pokemon.js") // get array of pokemon and store in variable
 
 app.use((req, res, next) => {
     console.log("I run on all routes!")
@@ -15,19 +17,41 @@ app.use((req, res, next) => {
 })
 app.use(expr.urlencoded({extended: false})) // allows the use of data from the body of the page
 app.use(expr.static("public")) // sets the default folder to search when selecting folders.
+app.use(express.json()) // get up so we can get json data from body
 app.use(methodOverride("_method")) // set up method for methodOverride
 
 /*============================================================
         Routes
 ============================================================*/
 
-// "/pokemon GET" - index TODO: Add basic layout forloop, add button for edit and delete
+// "/pokemon" GET - index TODO: Add basic layout forloop, add button for edit and delete
+app.get("/pokemon", (req, res) => {
+    
+    res.render("index",{
+        pokemon: arrPokemon            
+    })
+})
 
-// "/pokemon/:id GET" - show TODO: Will display all of the information about a pokemon
+// "/pokemon/:id" GET - show TODO: Will display all of the information about a pokemon
+app.get("/pokemon/:id", (req, res) => {
+    let indPokemon = req.params.id
+    res.render("show",{
+        pokemon: arrPokemon[indPokemon]
+    })
+})
 
-// "/pokemon/new GET" - new TODO: Add required info to create pokemon and push to array
+// "/pokemon/new" GET - new TODO: Add required info to create pokemon and push to array
+app.get("/pokemon/new", (req, res) => {
+    res.render("new")
+})
 
-// "/pokemon/:id/edit" - edit TODO: Pull existing data form selected pokemon and display it in form, allowing to edit (all into/ some info "haven't decided yet") and then save changes. Adding bodyData to array[id] and render index via POST after.
+// "/pokemon/:id/edit" - GET TODO: Pull existing data form selected pokemon and display it in form, allowing to edit (all into/ some info "haven't decided yet") and then save changes. Adding bodyData to array[id] and render index via POST after.
+app.get("/pokemon/:id/edit", (req, res) => {
+    let indPokemon = req.params.id
+    res.render("edit",{
+        pokemon: arrPokemon[indPokemon]
+    })
+})
 
 // "/pokemon" POST - index TODO: get index after adding pokemon from "/new" route
 
