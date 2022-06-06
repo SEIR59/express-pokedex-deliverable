a1993b4b9553dcc82cb43f26ea6bc3980b1fea9b
kmcaloney15 <katymcaloney@gmail.com>
2022-06-06 18:32:49 -0400

    index page is displaying index

--- server.js
+++ server.js
@@ -1,33 +1,32 @@
-const express    = require('express');
-const app        = express();
-const Pokemon = require('./pokedex/pokemon.js');
+const express = require("express");
+const app = require("liquid-express-views")(express());
+const Pokemon = require("./pokedex/pokemon.js");
 
+app.get("/", (req, res) => {
+  res.send("testing");
+});
 
-app.get('/', (req, res) => {
-    res.send('testing')
-})
+// INDEX
+app.get("/pokemon", (req, res) => {
+//   res.send("testing")
+    res.render("index", //index.Liquid
+  {
+    data: Pokemon,
+  });
+});
 
-
-// // INDEX
-// app.get('/', (req, res) => {
-// res.render('index.liquid', { data: Pokemon });
-// });
-
+// app.get("/pokemon/:id", (req, res) => {
+//     res.render("show", {
+//         pokemon:
+//     })
+// })
 
 // // SHOW
 // app.get('/:id', (req, res) => {
 // res.render('show.liquid', { data: Pokemon[req.params.id] });
 // });
 
-
-
-
-
-
-
-
-
 // PORT
 app.listen(3002, () => {
-    console.log("port 3002 is active")
-})
+  console.log("port 3002 is active");
+});
