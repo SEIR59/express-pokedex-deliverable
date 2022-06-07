ee36e2b0ebc8a057a597dea45850072dd1c4e509
kmcaloney15 <katymcaloney@gmail.com>
2022-06-07 15:48:31 -0400

    added methodOverride

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const express = require("express");
 const app = require("liquid-express-views")(express());
 const Pokemon = require("./pokedex/pokemon.js");
 
+//include the method-override package
+const methodOverride = require("method-override");
 
 //how we are connecting our css page
 app.use(express.static('public')); //tells express to try to match requests with files in the directory called 'public'
@@ -13,8 +15,9 @@ app.use(express.urlencoded({extended:false}));
 
 
 
-
-
+////////////////////////////////////////////
+// Routes
+////////////////////////////////////////////
 
 
 app.get("/", (req, res) => {
@@ -22,7 +25,7 @@ app.get("/", (req, res) => {
 });
 
 
-// INDEX
+// INDEX ROUTE
 app.get("/pokemon", (req, res) => {
 //   res.send("testing")
     res.render("index", //index.Liquid
@@ -31,11 +34,34 @@ app.get("/pokemon", (req, res) => {
   });
 });
 
+//NEW ROUTE
 app.get("/pokemon/new", (req, res) => {
     res.render('new')
 })
 
 
+//Create route
+//  create array, append each then push all / create three sections on form / could set it up where coma seperated then pops them into an array 
+app.post("/pokemon", (req, res) => {
+
+  //create new pokemon
+  Pokemon.push(req.body)
+  res.redirect('/pokemon')
+})
+
+//DELETE
+
+//PUT
+app.put("/pokemon/:index", (req, res) => {
+ 
+  Pokemon[req.params.index] = req.body; //in our fruits array, find the index that is specified in the url (:indexOfFruitsArray).  Set that element to the value of req.body (the input data)
+  res.redirect("/pokemon"); //redirect to the index page
+});
+
+
+
+
+// ID
 app.get("/pokemon/:id", (req, res) => {
     res.render("show", {
         data: Pokemon[req.params.id]
@@ -45,8 +71,6 @@ app.get("/pokemon/:id", (req, res) => {
 
 
 
-//Create route
-//  create array, append each then push all / create three sections on form / could set it up where coma seperated then pops them into an array 
 
 
 
