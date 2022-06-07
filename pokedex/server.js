5265cffeb519b0cb1390319dd0e130efd2f9a2e1
patwyzlic <patrickwyzlic@gmail.com>
2022-06-07 15:51:34 -0400

    New works

--- pokedex/server.js
+++ pokedex/server.js
@@ -12,14 +12,18 @@ app.use((req, res, next) => {
 
 app.use(express.urlencoded({extended:false}));
 
-
 app.get('/pokemon', (req, res) =>{
     res.render('index', {
         allPokemon: pokemon
     })
 });
 
-app.get('/pokemon/:id', (req, res) =>{
+app.post('/pokemon', (req, res) =>{
+    Pokemon.push(req.body);
+    res.redirect("/pokemon")
+});
+
+app.put('/pokemon/:id', (req, res) =>{
     res.render('show', {
         thePokemon: Pokemon[req.params.id]
     })
@@ -27,6 +31,25 @@ app.get('/pokemon/:id', (req, res) =>{
 
 app.get('/pokemon/new', (req, res) =>{
     res.render('new')
+});
+
+
+app.get('/pokemon/:id', (req, res) =>{
+    res.render('show', {
+        thePokemon: Pokemon[req.params.id]
+    })
+});
+
+app.get('/pokemon/:id/edit', (req, res) =>{
+    res.render('edit', {
+        thePokemon: Pokemon[req.params.id]
+    })
+});
+
+app.put('/pokemon/:id', (req, res) =>{
+    res.render('edit', {
+        thePokemon: Pokemon[req.params.id]
+    })
 });
 
 
