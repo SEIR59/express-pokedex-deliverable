530547e3394084bdab0c8a62c017b98eecc57231
kmcaloney15 <katymcaloney@gmail.com>
2022-06-06 21:43:09 -0400

    created path to new pokemon page

--- server.js
+++ server.js
@@ -2,29 +2,53 @@ const express = require("express");
 const app = require("liquid-express-views")(express());
 const Pokemon = require("./pokedex/pokemon.js");
 
+
+//how we are connecting our css page
+app.use(express.static('public')); //tells express to try to match requests with files in the directory called 'public'
+
+
+//near the top, around other app.use() calls
+app.use(express.urlencoded({extended:false}));
+
+
+
+
+
+
+
+
 app.get("/", (req, res) => {
   res.send("testing");
 });
+
 
 // INDEX
 app.get("/pokemon", (req, res) => {
 //   res.send("testing")
     res.render("index", //index.Liquid
   {
-    data: Pokemon, // liquid variable
+    data: Pokemon, // data liquid variable
   });
 });
 
-// app.get("/pokemon/:id", (req, res) => {
-//     res.render("show", {
-//         pokemon:
-//     })
-// })
+app.get("/pokemon/new", (req, res) => {
+    res.render('new')
+})
 
-// // SHOW
-// app.get('/:id', (req, res) => {
-// res.render('show.liquid', { data: Pokemon[req.params.id] });
-// });
+
+app.get("/pokemon/:id", (req, res) => {
+    res.render("show", {
+        data: Pokemon[req.params.id]
+    })
+})
+
+
+
+
+//Create route
+//  create array, append each then push all / create three sections on form / could set it up where coma seperated then pops them into an array 
+
+
 
 // PORT
 app.listen(3002, () => {
