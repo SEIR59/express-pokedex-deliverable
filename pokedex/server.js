ddc401f536beacc5500f2aa489b60bdfc4495e9a
bhenderson93 <brycehenderson1993@gmail.com>
2022-06-07 13:17:57 -0700

    All pages and routes set up and functional. Everything linked, just need to populate

--- pokedex/server.js
+++ pokedex/server.js
@@ -3,26 +3,28 @@ const app = require('liquid-express-views')(express())
 const PORT = 3000
 const pokemonList = require('./pokemon.js')
 
+app.use(express.urlencoded({extended:false}));
+app.use(express.static('public')); //tells express to try to match requests with files in the directory called 'public'
+
 app.get('/' , (req,res)=>{
     res.redirect('/pokemon')
 })
 
 app.get('/pokemon',(req,res)=>{
-    res.render('index',{
+    res.render('pokemonLiquid/index',{
         pokemonList: pokemonList
     })})
 
+    app.get('/pokemon/new',(req,res)=>{
+        res.render('pokemonLiquid/new' )
+    })
 
 app.get('/pokemon/:id',(req,res)=>{
-    
-})
-
-app.get('/pokemon/new',(req,res)=>{
-    
+    res.render('pokemonLiquid/show')
 })
 
 app.get('/pokemon/:id/edit',(req,res)=>{
-    
+    res.render('pokemonLiquid/edit')
 })
 
 app.post('/pokemon',(req,res)=>{
