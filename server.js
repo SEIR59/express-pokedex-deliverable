e2806216e81ae6ceb6364fbe0dab790f86b449f6
Edgie Manzo <104868823+emanzo14@users.noreply.github.com>
2022-06-07 14:51:03 -0400

    added data to pokedex

--- server.js
+++ server.js
@@ -1,8 +1,25 @@
 const express = require('express')
 const app = require('liquid-express-views')(express())
-const PokemonList = require('./models/pokemon.js')
+const PokemonList = require('./models/pokedex/pokemon.js')
+const pokemon = require('./models/pokedex/pokemon.js')
 
 let port = 3000
 app.listen(port, () => {
     console.log("Listining on port: ", port)
 }) 
+
+
+app.get('/', (req, res) => {
+    res.render(
+        'index',
+        {
+            PokemonList: PokemonList
+        }
+    )
+}) 
+
+app.get('/:id', (req, res) => {
+    res.render('pokemon', {
+        pokemon: PokemonList[req.params.id]
+    })
+})
