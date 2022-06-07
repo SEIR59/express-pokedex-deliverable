ae717908f4502cea8c0307309ce8f15551aadd95
heysungj <linjing0525@gmail.com>
2022-06-06 21:11:20 -0400

    added put and delete functions

--- server.js
+++ server.js
@@ -1,19 +1,21 @@
-const pokemons = require('./pokedex/models/pokemon.js');
-const express = require('express');
-const { redirect } = require('express/lib/response');
+const pokemons = require("./pokedex/models/pokemon.js");
+const express = require("express");
+const { redirect } = require("express/lib/response");
 const app = require("liquid-express-views")(express());
-const methodOverride = require('method-override');
+const methodOverride = require("method-override");
 
 // use methodOverride, will add query parameter to our delete form named _method
-app.use(methodOverride('_method'));
+app.use(methodOverride("_method"));
 // allows us to view body of a post request
-app.use(express.urlencoded({
-    extended: false
-}))
+app.use(
+  express.urlencoded({
+    extended: false,
+  })
+);
 //tells express to try to match requests with files in the directory called 'public'
-app.use(express.static('public'));
+app.use(express.static("public"));
 // This prepares our api to receive json data from the body of all incoming requests.
-app.use(express.json())
+app.use(express.json());
 
 // Index
 // GET /pokemon
@@ -30,38 +32,45 @@ app.use(express.json())
 // Destroy
 // DELETE /pokemon/:id
 
-app.get('/pokemon', (req, res) => {
-    res.render('index', {
-        allPokemons: pokemons
-    })
+app.get("/pokemon", (req, res) => {
+  res.render("index", {
+    allPokemons: pokemons,
+  });
 });
 
-app.get('/pokemon/:id', (req, res) => {
-    res.render('show', {
-        pokemon: pokemons[req.params.id]
-    })
+app.get("/pokemon/new", (req, res) => {
+  res.render("new", {});
 });
 
-app.get('/pokemon/new', (req, res) => {
-    
+app.get("/pokemon/:id", (req, res) => {
+  res.render("show", {
+    pokemon: pokemons[req.params.id],
+  });
 });
 
-app.post('/pokemon', (req, res) => {
-
+app.post("/pokemon", (req, res) => {
+  console.log(req.body);
+  pokemons.push(req.body);
+  res.redirect("/pokemon");
 });
 
-app.get('/pokemon/:id/edit', (req, res) => {
-
+app.get("/pokemon/:id/edit", (req, res) => {
+  res.render("edit", {
+    pokemon: pokemons[req.params.id],
+    index: req.params.id,
+  });
 });
 
-app.put('/pokemon/:id', (req, res) => {
-
+app.put("/pokemon/:id", (req, res) => {
+  pokemons[req.params.id] = req.body;
+  res.redirect("/pokemon");
 });
 
-app.delete('/pokemon/:id', (req, res) => {
-
+app.delete("/pokemon/:id", (req, res) => {
+  pokemons.splice(req.params.id, 1);
+  res.redirect("/pokemon");
 });
 
 app.listen(3000, () => {
-    console.log("listening on port 3000!");
-})
+  console.log("listening on port 3000!");
+});
