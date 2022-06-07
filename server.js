4f6ffe1236fd47f6fb4ce21d1f0a1e99c59578dd
NSTaft <taftnathan13@gmail.com>
2022-06-07 14:09:40 -0500

    Set up variables in require statements in server.js

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
 const express    = require('express');
-const app        = express();
-const Pokemon = require('../models/pokemon.js');
+const app = require('liquid-express-views')(express());
+const Pokemon = require('./models/pokemon.js');
+const methodOverride = require('method-override')
+const rowdy = require('rowdy-logger')
+const routeReport = rowdy.begin(app)
+const port = 3000
 
 // INDEX
 app.get('/', (req, res) => {
