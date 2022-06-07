55b8619260f36d853ad593b83d97f09865e8c96c
hnhtran <tran_eetas@yahoo.com>
2022-06-07 19:08:00 +1000

    prepare files

---
+++ server.js
@@ -0,0 +1,15 @@
+const express    = require('express');
+const app        = express();
+const Pokemon = require('../models/pokemon.js');
+
+
+// INDEX
+app.get('/', (req, res) => {
+res.render('index.liquid', { data: Pokemon });
+});
+
+
+// SHOW
+app.get('/:id', (req, res) => {
+res.render('show.liquid', { data: Pokemon[req.params.id] });
+});
