fa228b8942c71d0d129c42f42135cf323555d8e2
hnhtran <tran_eetas@yahoo.com>
2022-06-07 19:27:12 +1000

    prepare basic server.js

--- server.js
+++ server.js
@@ -1,15 +1,48 @@
-const express    = require('express');
-const app        = express();
-const Pokemon = require('../models/pokemon.js');
+const express = require("express");
+const app = require("liquid-express-views")(express());
+const methodOverride = require("method-override");
+const Pokemon = require("../models/pokemon.js");
+const routesReport = rowdy.begin(app);
+const port = 3000
 
+// middleware
+// allows us to view body of a post request
+// allow us to access data that posting. Can be use add, edit, put, etc. i.e. allow developers use more than just get and post route
+app.use(
+  express.urlencoded({
+    extended: false,
+  })
+);
+// tells express to try to match requests with files in the directory called 'public'
+app.use(express.static("public"));
+// This prepares our api to receive json data from the body of all incoming requests.
+// Do we need this line?
+app.use(express.json());
+
+// positioning: as long as it aheads of the route, we will be fine
+app.use(methodOverride("_method"));
 
 // INDEX
 app.get('/', (req, res) => {
-res.render('index.liquid', { data: Pokemon });
-});
-
+    res.render(
+        'index',
+        {
+            data: Pokemon
+        }
+    )
+})
 
 // SHOW
 app.get('/:id', (req, res) => {
-res.render('show.liquid', { data: Pokemon[req.params.id] });
-});
+    res.render(
+        'show',
+        {
+            data: Pokemon[req.params.id]
+        }
+    )
+})
+
+app.listen(port, () => {
+    console.log('port 3000 listens')
+    routesReport.print()
+})
