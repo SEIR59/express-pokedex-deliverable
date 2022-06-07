c5f186e723aed49991a960f5b3a0937aa41b6614
kmcaloney15 <katymcaloney@gmail.com>
2022-06-07 17:28:23 -0400

    able to delete pokemon

--- server.js
+++ server.js
@@ -10,8 +10,11 @@ const Pokemon = require("./pokedex/pokemon.js");
 //include the method-override package
 const methodOverride = require("method-override");
 
+//MIDDLEWARE
+
 //how we are connecting our css page
 app.use(express.static('public')); //tells express to try to match requests with files in the directory called 'public'
+app.use(methodOverride("_method"))
 
 
 //near the top, around other app.use() calls
@@ -24,28 +27,36 @@ app.use(express.urlencoded({extended:false}));
 // Routes
 ////////////////////////////////////////////
 
-
-app.get("/", (req, res) => {
-  res.send("testing");
-});
-
-
 // INDEX ROUTE
 app.get("/pokemon", (req, res) => {
-//   res.send("testing")
-    res.render("index", //index.Liquid
+  //   res.send("testing")
+  res.render("index", //index.Liquid
   {
     data: Pokemon, // data is liquid variable
   });
 });
 
-//NEW ROUTE
+// NEW - GET
 app.get("/pokemon/new", (req, res) => {
-    res.render('new')
+  res.render('new')
 })
 
+//DELETE
+app.delete("/pokemon/:id", (req, res) => {
+  Pokemon.splice(req.params.id, 1), //removes 1 item from the array
+  console.log('delete is running'),
+  res.redirect("/pokemon"); //redirects back to the index page
+})
 
-//Create route
+//UPDATE - PUT
+app.put("/pokemon/:index", (req, res) => {
+  
+  Pokemon[req.params.index] = req.body; //in our fruits array, find the index that is specified in the url (:indexOfFruitsArray).  Set that element to the value of req.body (the input data)
+  res.redirect("/pokemon"); //redirect to the index page
+});
+
+
+// CREATE - POST
 //  create array, append each then push all / create three sections on form / could set it up where coma seperated then pops them into an array 
 app.post("/pokemon", (req, res) => {
 
@@ -58,25 +69,16 @@ app.post("/pokemon", (req, res) => {
 
 
 
-//DELETE
-
-//PUT
-app.put("/pokemon/:index", (req, res) => {
-  
-  Pokemon[req.params.index] = req.body; //in our fruits array, find the index that is specified in the url (:indexOfFruitsArray).  Set that element to the value of req.body (the input data)
-  res.redirect("/pokemon"); //redirect to the index page
-});
-
-
-//EDIT
+//EDIT - GET
 app.get("/pokemon/:index/edit", (req, res) => {
-    res.render("edit", 
-    {
-       pokemon: Pokemon[req.params.index], //the pokemon object
-       index: req.params.index, //index of the array
-    })
-    console.log('edit route is working')
+  res.render("edit", 
+  {
+    data: Pokemon[req.params.index], //the pokemon object
+    index: req.params.index, //index of the array
   })
+  console.log('edit route is working')
+})
+
 
 
 // SHOW
@@ -86,11 +88,6 @@ app.get("/pokemon/:index", (req, res) => {
         index: req.params.index
     })
 })
-
-
-
-
-
 
 
 // PORT
