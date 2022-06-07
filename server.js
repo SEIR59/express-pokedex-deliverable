0b811463871807ab0bf0ac6d9503384a2b33c62f
tmsheipline <taylorsheipline_2012@hotmail.com>
2022-06-06 21:06:49 -0400

    Server set up and listening. Liquid files boilerplates created. Index route finished

---
+++ server.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const app = require("liquid-express-views")(express());
+const port = 3000;
+const pokemon = require("./pokedex/pokemon");
+
+//middleware - must always be at the top
+app.use((req, res, next) => {
+  console.log(`I run for all routes`);
+  next();
+});
+//express function body parser middleware
+app.use(express.urlencoded({ extended: false }));
+
+app.use(express.static("public")); //tells express to try to match requests with files in the directory called 'public'
+
+
+// INDEX
+app.get("/pokemon", (req, res) => {
+  res.render("index", { allPokemon: pokemon });
+});
+
+// SHOW
+app.get("/:id", (req, res) => {
+  res.render("show.liquid", { data: Pokemon[req.params.id] });
+});
+
+app.listen(port, () => {
+  console.log(`Listening`);
+});
