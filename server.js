253956de70a2b95cc7b361d5f9e22c19325167b2
idbic <dbicga@gmail.com>
2022-06-08 06:12:39 -0500

    created new/ post and put routes. Successfully adding new Pokemon

--- server.js
+++ server.js
@@ -18,6 +18,16 @@ app.get('/pokemon', (req, res) => {
 app.get('/pokemon/new', (req, res) => {
   res.render('new')
 })
+
+//Post Route
+
+app.post('/pokemon', (req, res) => {
+  
+  Pokemon.push(req.body);
+  console.log(req.body);
+  res.redirect('/pokemon'); //send the user back to /index
+});
+
 // SHOW
 
 app.get('/pokemon/:id', (req, res) => {
@@ -29,6 +39,24 @@ app.get('/pokemon/:id', (req, res) => {
 })
 
 
+//edit route
+app.get('/pokemon/:id/edit', (req, res) => {
+  res.render(
+      'edit',
+      {
+          data: Pokemon[req.params.id], 
+          index: req.params.id
+      })
+      
+})
+
+app.put('/pokemon/:id', (req, res) => {
+  
+  Pokemon[req.params.id] = req.body
+  
+})
+
+//Listener
 app.listen(port, (req, res) => {
     console.log('We are live from 3000, the pokemon center')
 })
