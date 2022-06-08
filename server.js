0cf5d6d397c1a91944259da942b744c7f5e0e18b
Edgie Manzo <104868823+emanzo14@users.noreply.github.com>
2022-06-08 09:04:05 -0400

    added the function of adding pokemon

--- server.js
+++ server.js
@@ -8,6 +8,28 @@ app.listen(port, () => {
     console.log("Listining on port: ", port)
 }) 
 
+app.post('/create', (req, res) => {
+
+    PokemonList.push({
+        id: req.body.id,
+        name:  req.body.name,
+        img:  req.body.img,
+        type:  req.body.type.split(", "),
+        stats:  {
+            hp: req.body.statshp, 
+            attack: req.body.statsattack, 
+            defense: req.body.statsdefense,
+        }
+
+    })
+    res.redirect('/')
+})
+
+app.get('/add', (req, res) => {
+    res.render('add', {
+        lastIndex: PokemonList.length
+    })
+})
 
 app.get('/', (req, res) => {
     res.render(
@@ -18,12 +40,10 @@ app.get('/', (req, res) => {
     )
 }) 
 
+
 app.get('/:id', (req, res) => {
     res.render('pokemon', {
         pokemon: PokemonList[req.params.id]
     })
 })
 
-app.get('/add', (req, res) => {
-    res.render('add')
-})
