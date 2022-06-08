ae02a3f882685fc0ff2e8d6db46c199106b7a6e4
jrbreeden <jrbreeden55@gmail.com>
2022-06-07 20:22:54 -0400

    created new, show, and edit routes

--- pokedex/server.js
+++ pokedex/server.js
@@ -1,23 +1,41 @@
 const express = require('express');
 const Pokemon = require('./models/pokemon.js')
 const app = require("liquid-express-views")(express())
-
+app.use(express.urlencoded({
+    extended: false
+}))
 // INDEX
 app.get('/', (req, res) => {
     res.render('index', { data: Pokemon });
-    });
-    
-    
-    // SHOW
-    app.get('/:id', (req, res) => {
-    res.render('show.liquid', { data: Pokemon[req.params.id] });
-    });
-    
-    
+});
+
+app.get('/pokemon/new', (req, res) => {
+    res.render('new')
+})
+app.post('/pokemon', (req, res) => {
+    Pokemon.push(req.body);
+    console.log(req.body);
+    res.redirect('/')
+})
+
+// SHOW
+app.get('/:id', (req, res) => {
+    res.render('show.liquid', { data: Pokemon[req.params.id],index: req.params.id })
+})
+
+app.get("/pokemon/:index/edit", (req, res) => {
+    res.render(
+      "edit", //render views/edit.liquid
+      {
+        //pass in an object that contains
+        pokemon: Pokemon[req.params.index],
+        index: req.params.index }
+    )
+  })
 
 
 
 
-    app.listen(3000, () => {
-        console.log("listening on port 3000!")
-    })
+app.listen(3000, () => {
+    console.log("listening on port 3000!")
+})
