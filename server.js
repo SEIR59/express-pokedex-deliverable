5e63bfbab88a645399ea5c82f340ba6614f32e18
annakoanna <anna.231991k@gmail.com>
2022-06-07 21:01:48 -0700

    Has separate show pages for each Pokémon

--- server.js
+++ server.js
@@ -16,8 +16,8 @@ app.use((req, res, next) => {
     next();
 });
 
-//const methodOverride = require("method-override");
-//app.use(methodOverride("_method"));
+const methodOverride = require("method-override");
+app.use(methodOverride("_method"));
 
 // INDEX
 app.get('/pokemon', (req, res) => {
@@ -26,24 +26,43 @@ app.get('/pokemon', (req, res) => {
 
 
 // SHOW
-app.get('/pokemon/:id', (req, res) => {
-    res.render('show', { data: Pokemon[req.params.id] });
-});
-
-//app.get('/pokemon/:id/edit', (req, res) => {
-    // res.render('edit', { data: Pokemon[req.params.id] });
- //});
- 
- 
- app.get('/pokemon/new', (req, res)=>{
-     res.render('new')
- })
-
-app.delete('/pokemon/:id', (req, res) => {
-    //remove item from array
-    pokemon.splice(req.params.id, 1)
-    res.redirect('/pokemon')//redirect back to index 
-})
+app.get('/pokemon/:id/edit', (req, res) => {
+    res.render('edit',
+     { data: Pokemon[req.params.id], 
+        index: req.params.id});
+    });
+    
+    //app.get('/pokemon/:id/edit', (req, res) => {
+        // res.render('edit', { data: Pokemon[req.params.id] });
+        //});
+        
+        app.get('/pokemon/new', (req, res)=>{
+            res.render('new')
+        })
+        app.delete('/pokemon/:id', (req, res) => {
+            //remove item from array
+            Pokemon.splice(req.params.id, 1)
+            res.redirect('/pokemon')//redirect back to index 
+        })
+        
+        app.post('/pokemon', (req, res) => {
+            Pokemon.push(req.body)
+            // pokemon += req.body.id
+            console.log(req)
+            res.redirect('/pokemon')
+        })
+        
+        app.put('/pokemon/:id',(req, res)=>{
+            console.log(req.body)
+            Pokemon[req.params.id]=req.body
+            res.redirect('/pokemon')
+            
+        })
+        
+        
+        app.get('/pokemon/:id', (req, res) => {
+            res.render('show', { data: Pokemon[req.params.id] });
+        });
 
 
 
