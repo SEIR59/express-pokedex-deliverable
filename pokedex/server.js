bd45b12e81af3c7ffd5d46065e06990015c80ee9
patwyzlic <patrickwyzlic@gmail.com>
2022-06-07 22:33:00 -0400

    Creating easier URLs

--- pokedex/server.js
+++ pokedex/server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const methodOverride = require('method-override')
-const pokemon = require('./pokemon');
+const Pokemon = require('./pokemon');
 const app = require("liquid-express-views")(express());
 
 const port = 3000;
@@ -15,56 +15,48 @@ app.use((req, res, next) => {
 
 app.use(express.urlencoded({extended:false}));
 
-app.get('/pokemon', (req, res) =>{
+app.get('/', (req, res) =>{
     res.render('index', {
-        allPokemon: pokemon
+        allPokemon: Pokemon
     })
 });
 
-app.post('/pokemon', (req, res) =>{
-    pokemon.push(req.body);
+app.post('/', (req, res) =>{
+    Pokemon.push(req.body);
     res.redirect("/pokemon")
 });
 
-app.get('/pokemon/:id', (req, res) =>{
-    res.render('show', {
-    })
-});
-
-app.post('/pokemon/:id', (req, res) =>{
-    res.render('show', {
-        thePokemon: pokemon[req.params.id]
-    })
-});
-
-app.get('/pokemon/new', (req, res) =>{
+app.get('/new', (req, res) =>{
     res.render('new')
 });
 
-
-app.get('/pokemon/:id', (req, res) =>{
+app.get('/:id', (req, res) =>{
     res.render('show', {
-        thePokemon: pokemon[req.params.id]
+        thePokemon: Pokemon[req.params.id]
     })
 });
 
-app.post('/pokemon/:id', (req, res) =>{
+app.post('/:id', (req, res) =>{
+    res.render('show', {
+        
+    })
+});
+
+app.get('/:id/edit', (req, res) =>{
     res.render('edit', {
-        thePokemon: pokemon[req.params.id]
+        thePokemon: Pokemon[req.params.id]
     })
 });
 
-
-app.get('/pokemon/:id/edit', (req, res) =>{
-    res.render('edit', {
-        thePokemon: pokemon[req.params.id]
-    })
-});
-
-app.post('/pokemon/:id/edit', (req, res) => {
-    pokemon[req.params.id]
+app.post('/:id/edit', (req, res) => {
+    thePokemon: Pokemon[req.params.id]
     res.redirect('/pokemon')
 })
+
+/*app.delete('/pokemon/:id', (req, res) =>{
+    Pokemon.splice(request.params.id, 1)
+    res.redirect('/pokemon')
+})*/
 
 app.listen(port, () => {
     console.log('listening');
