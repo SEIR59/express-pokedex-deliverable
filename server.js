056d9f5e673bb6dade662e163bc4197cc0078b28
Sal Dragone <104164651+sdragone01@users.noreply.github.com>
2022-06-16 13:37:17 -0400

    added update route

--- server.js
+++ server.js
@@ -5,12 +5,38 @@ app.use(express.static('public'))
 const pokemon = require('./pokedex/pokemon.js')
 const methodOverride = require("method-override");
 app.use(methodOverride("_method"));
+
+app.get('/',(req,res)=>{
+    res.send('hello world')
+})
+
+//edit
+app.get("/:id/edit",(req,res)=>{
+  
+    console.log("were editing")
+    res.render(
+        "edit",
+        {
+            index: req.params.id,
+            pokemon: pokemon[req.params.id]
+        })
+    })
+
+
+app.put("/pokemon/:id", (req, res) => {
+    pokemon[req.params.id] = req.body; 
+    res.redirect("/pokemon"); 
+  });
+
+//index
 app.get('/pokemon',(req,res)=>{
     res.render('index.liquid',
     {
         pokemonList:pokemon
     })
 })
+
+//new
 app.get('/pokemon/new',(req,res)=>{
     res.render('new')
 })
@@ -20,51 +46,31 @@ app.post('/pokemon', (req, res)=>{
     res.redirect('/pokemon'); 
 })
 
-app.get('/',(req,res)=>{
-    res.send('hello world')
-})
+//consolelog
+app.use((req, res, next) => {
+    console.log('I run for all routes');
+    next();
+});
 
-app.put("/pokemon/:id", (req, res) => {
- 
-    pokemon[req.params.id] = req.body; 
-    res.redirect("/pokemon"); 
+//delete
+app.delete("/pokemon/:id", (req, res) => {
+    pokemon.splice(req.params.id, 1); //remove the item from the array
+    res.redirect("/pokemon"); //redirect back to index route
   });
 
-
+//show route
 app.get('/pokemon/:id', (req,res)=>{
 
     res.render('show',
     {
         pokemonList:pokemon[req.params.id]
     })
+
 })
-
-
-
-
-app.use((req, res, next) => {
-    console.log('I run for all routes');
-    next();
-});
-app.delete("/pokemon/:id", (req, res) => {
-    pokemon.splice(req.params.id, 1); //remove the item from the array
-    res.redirect("/pokemon"); //redirect back to index route
-  });
-
-  app.get("/pokemon/:id/edit", (req,res)=>{
-    res.render(
-        "edit", 
-    {
-        pokemon: pokemon[req.params.id],
-        index: req.params.id,
-    })
-})
-
-
 
 
 
 
 app.listen(4000,()=>{
-    console.log("listning to port 3000")
+    console.log("listning to port 4000")
 })
